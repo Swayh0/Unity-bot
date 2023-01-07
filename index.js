cc1c32ba7a19d4e3f5bb803fac83ad0eb4d495cb
Swayh0 <51081260+Swayh0@users.noreply.github.com>
2023-01-07 22:37:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { User, Message, GuildMember, ThreadMember } = Partials
 
 const client = new Client({ 
     intents: [Guilds, GuildMembers, GuildMessages],
-    Partials: [User, Message, GuildMember, ThreadMember]
+    Partials: [User, Message, GuildMember, ThreadMember,]
 });
 
 
@@ -21,6 +21,40 @@ loadEvents(client);
 client.login(client.config.token)
 .then(() => {
     console.log(`Client logged in as ${client.user.tag}`);
-    client.user.setActivity(`with ${client.guilds.cache.size} guilds`);
-})
+    const activities = [
+      
+        { name: `/chess-info`, type: 3 },
+
+        { name: `${client.guilds.cache.size} Servers`, type: 5 },
+
+        { name: `${client.users.cache.size} Users`, type: 4 },
+    
+        //Alle types
+        // 0 = Playing
+        // 1 = Streaming
+        // 2 = Listening	
+        // 3 = Watching
+        // 4 = Custom
+        // 5 = Competing
+
+        ];
+        const status = [
+            'online',
+            'dnd',
+            'idle',
+        ];
+        let i = 0;
+        setInterval(() => {
+            if(i >= activities.length) i = 0
+            client.user.setActivity(activities[i])
+            i++;
+        }, 5000);
+      
+        let s = 0;
+        setInterval(() => {
+            if(s >= activities.length) s = 0
+            client.user.setStatus(status[s])
+            s++;
+        }, 5000);
+    })
 .catch((err) => console.log(err));
